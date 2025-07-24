65c7968f9a5be73a7e8afb121becec14e15286ae
Mindblowinggg <akahar36@gmail.com>
2025-07-24 16:39:59 +0530

    gg

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,6 +5,9 @@ export const Hero = () => {
   return (
     <div className="main">
     <img src="/herotext.png" alt="" />
+    <p>Are you an artist?</p>
+   <button className="btn"><i className="animation"></i>Find Musicians<i className="animation"></i>
+    </button>
     </div>
   );
 };
