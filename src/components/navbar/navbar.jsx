615995cd7d581d3908fbdb116c6e083d7f4f806d
Mindblowinggg <akahar36@gmail.com>
2025-07-24 13:25:47 +0530

    n

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='navlogo'>
+      <h1>DhunDost</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar;
