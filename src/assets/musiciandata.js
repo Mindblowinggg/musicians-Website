22c5a4e6fa74373c18e59273b9b28c1b61eddec0
Mindblowinggg <akahar36@gmail.com>
2025-07-27 12:31:38 +0530

    kjldf

--- src/assets/musiciandata.js
+++ src/assets/musiciandata.js
@@ -10,7 +10,7 @@ const musiciansData = [
     experience: "5 years",
     genre: "Rock, bollywood",
     contact: "aman.k@email.com",
-    pfp: "/pfp/amanpfp.JPG",
+    pfp: "/pfp/amanpfp.jpg",
     experiencedDescription: "Aman Kahar, with 5 years of dedication, brings a powerful vocal presence to every performance. Specializing in Rock and Bollywood, he has graced numerous stages, delivering captivating interpretations and high-energy shows that resonate deeply with audiences. His commitment to musical excellence ensures an unforgettable experience.",
     readyToHelpDescription: "Aman is ready to collaborate for live performances, studio sessions, and events. He offers versatile vocal services tailored to your project. Contact Aman to create extraordinary musical moments together!"
   },
@@ -24,7 +24,7 @@ const musiciansData = [
     experience: "5 years",
     genre: "Classical, Bollywood",
     contact: "manav.b@email.com",
-    pfp: "/pfp/manavpfp.JPG",
+    pfp: "/pfp/manavpfp.jpg",
     experiencedDescription: "Manav Bhatt, a vocalist with 5 years of experience, expertly blends Classical and Bollywood genres. His performances are known for their soulful depth and technical precision, offering a unique and enriching musical journey for listeners. He has a strong command over complex melodies and ragas.",
     readyToHelpDescription: "Manav is available for classical concerts, Bollywood events, private functions, and studio recordings. He is committed to delivering professional and heartfelt vocal performances. Reach out to Manav for a truly authentic musical experience."
   },
@@ -34,11 +34,11 @@ const musiciansData = [
     name: "Vera Platon",
     instrument: ["Vocal"],
      country: "Romania",
-    state: "Iași County", // Capital 'I' and 'ș'
+    state: "Iași County", 
     city: "Iași",  
     experience: "5 years",
     genre: "Jazz, rock",
-    contact: "vera.m@email.com",
+    contact: "vera@email.com",
     pfp: "/pfp/verapfp.jpg",
     experiencedDescription: "Vera Platon, an accomplished vocalist from Romania with 5 years of experience, effortlessly navigates the worlds of Jazz and Rock. Her powerful and versatile voice, combined with her dynamic stage presence, makes every performance a thrilling experience. She has a talent for improvisation and connecting with the rhythm.",
     readyToHelpDescription: "Vera is available for international gigs, jazz club performances, rock band collaborations, and studio recording sessions. She is dedicated to delivering high-quality, impactful vocal work. Get in touch with Vera for professional musical contributions."
